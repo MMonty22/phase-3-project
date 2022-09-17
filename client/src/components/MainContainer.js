40223cc872eb814c584d067bf7ced22ae6c3ae3d
Michael <michael.c.montgomery@gmail.com>
2022-09-17 14:13:44 -0500

    added data map and bet comp to MainContainer comp

--- client/src/components/MainContainer.js
+++ client/src/components/MainContainer.js
@@ -1,9 +1,12 @@
 import React from "react";
+import Bet from "./Bet";
 
-function MainContainer({}) {
+function MainContainer({bets}) {
+    const data = bets.map((betInfo) => <Bet key={betInfo.id} betInfo={betInfo}/>)
+
     return (
         <div>
-
+            <ul>{data}</ul>
         </div>
     )
 }
