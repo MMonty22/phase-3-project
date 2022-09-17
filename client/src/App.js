556df640179a560adaa9f834f1de81ef3817671e
Michael <michael.c.montgomery@gmail.com>
2022-09-17 14:11:23 -0500

    added useEffect, state and more to App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react'
+import {Routes, Route} from 'react-router-dom'
+import NavBar from "./components/NavBar"
+import MainContainer from "./components/MainContainer"
 
 function App() {
+  const [bets, setBets] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:9292/people')
+    .then(res => res.json())
+    .then(data => {
+      console.log(data)
+      setBets(data)
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="header">Live on the Line Bet Tracker</h1>
+      <NavBar />
+      <Routes>
+        <Route path="/Bets">
+          <MainContainer bets={bets}/>
+        </Route>
+      </Routes>
     </div>
   );
 }
