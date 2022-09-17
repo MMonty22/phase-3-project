8afbf2a7d1d9c76d0074630add3dc5230ee4185e
Michael <michael.c.montgomery@gmail.com>
2022-09-17 14:15:03 -0500

    added elements to Bet component

--- client/src/components/Bet.js
+++ client/src/components/Bet.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-function Bet({}) {
-    return (
-        <div>
+function Bet({betInfo}) {
+    const {description, odds, league, betType, result, unitsChange, segment} = betInfo
 
+    return (
+        <div className="betInfo">
+            <h3>{description} {odds}</h3>
+            <p>{league}</p>
+            <p>{betType}</p>
+            <p>{result}</p>
+            <p>{unitsChange}</p>
+            <p>{segment}</p>
         </div>
     )
 }
