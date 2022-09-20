69d6ce9a16c211feae4d532c0fbdd43adad6f240
Michael <michael.c.montgomery@gmail.com>
2022-09-20 16:23:54 -0500

    added states to form component

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,27 +1,38 @@
 import React, {useState} from "react";
 
 function Form() {
+    const [formData, setFormData] = useState({
+        personName: "",
+        bet: "",
+        odds: "",
+        league: "",
+        result: "",
+        unitsChange: "",
+        segment: ""
+    })
+    const [checked, setChecked] = useState(false)
+
 
     return (
         <div className="form">
             <h2>Add A Bet</h2>
             <form>
                 <label>Person</label>
-                <input type="text" name="name" placeholder="Person's Name" value={} onChange={}></input>
+                <input id="personName" type="text" name="name" placeholder="Person's Name" value={formData.personName} onChange={}></input>
                 <label>Bet</label>
-                <input type="text" name="bet" placeholder="Ex: Cubs Moneyline vs White Sox" value={} onChange={}></input>
+                <input id="bet" type="text" name="bet" placeholder="Ex: Cubs Moneyline vs White Sox" value={formData.bet} onChange={}></input>
                 <label>Odds</label>
-                <input type="text" name="odds" placeholder="-110" value={} onChange={}></input>
+                <input id="odds" type="text" name="odds" placeholder="-110" value={formData.odds} onChange={}></input>
                 <label>League</label>
-                <input type="text" name="league" placeholder="MLB" value={} onChange={}></input>
+                <input id="league" type="text" name="league" placeholder="MLB" value={formData.league} onChange={}></input>
                 <label>Result</label>
-                <input type="text" name="result" placeholder="Leave blank if no result" value={} onChange={}></input>
+                <input id="result" type="text" name="result" placeholder="Leave blank if no result" value={formData.result} onChange={}></input>
                 <label>Units Won or Lost</label>
-                <input type="text" name="units_change" placeholder="Ex: +1.3 or -1.15" value={} onChange={}></input>
+                <input id="unitsChange" type="text" name="units_change" placeholder="Ex: +1.3 or -1.15" value={formData.unitsChange} onChange={}></input>
                 <label>Segment</label>
-                <input type="text" name="segment" placeholder="Ex: Take It to the Bank" value={} onChange={}></input>
+                <input id="segment" type="text" name="segment" placeholder="Ex: Take It to the Bank" value={formData.segment} onChange={}></input>
                 <label>Add Another Bet?</label>
-                <input id="checkbox" type="checkbox" checked={} onChange={}></input>
+                <input id="checkbox" type="checkbox" checked={checked} onChange={}></input>
             </form>
         </div>
     )
