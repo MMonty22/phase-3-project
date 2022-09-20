04c354224b3e2346d383efc131d3e436ea8907b5
Michael <michael.c.montgomery@gmail.com>
2022-09-20 16:14:50 -0500

    added to form component

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Form() {
 
     return (
-        <div>
-
+        <div className="form">
+            <h2>Add A Bet</h2>
+            <form>
+                <label>Person</label>
+                <input type="text" name="name" placeholder="Person's Name" value={} onChange={}></input>
+                <label>Bet</label>
+                <input type="text" name="bet" placeholder="Ex: Cubs Moneyline vs White Sox" value={} onChange={}></input>
+                <label>Odds</label>
+                <input type="text" name="odds" placeholder="-110" value={} onChange={}></input>
+                <label>League</label>
+                <input type="text" name="league" placeholder="MLB" value={} onChange={}></input>
+                <label>Result</label>
+                <input type="text" name="result" placeholder="Leave blank if no result" value={} onChange={}></input>
+                <label>Units Won or Lost</label>
+                <input type="text" name="units_change" placeholder="Ex: +1.3 or -1.15" value={} onChange={}></input>
+                <label>Segment</label>
+                <input type="text" name="segment" placeholder="Ex: Take It to the Bank" value={} onChange={}></input>
+                <label>Add Another Bet?</label>
+                <input id="checkbox" type="checkbox" checked={} onChange={}></input>
+            </form>
         </div>
     )
 }
