509b1ae08c317a98733fe385ff58dbfc4cfa7f5f
Michael <michael.c.montgomery@gmail.com>
2022-09-21 17:45:50 -0500

    added ability to submit 2 bets at once with addForm state

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -11,6 +11,7 @@ function Form({addBet}) {
         segment: ""
     })
     const [checked, setChecked] = useState(false)
+    const [addForm, setAddForm] = useState(false)
 
     function handleSubmit(event) {
         event.preventDefault()
@@ -43,6 +44,11 @@ function Form({addBet}) {
 
     function handleCheck() {
         setChecked(!checked)
+        handleAddForm()
+    }
+
+    function handleAddForm() {
+        setAddForm(!addForm)
     }
 
     return (
@@ -67,6 +73,26 @@ function Form({addBet}) {
                 <label>Add Another Bet?</label>
                 <input id="checkbox" type="checkbox" checked={checked} onChange={handleCheck}></input>
             </form>
+            {addForm && 
+            <form onSubmit={handleSubmit}>
+                <label>Person</label>
+                <input id="personName" type="text" name="name" placeholder="Person's Name" value={formData.personName} onChange={handleChange}></input>
+                <label>Bet</label>
+                <input id="bet" type="text" name="bet" placeholder="Ex: Cubs Moneyline vs White Sox" value={formData.bet} onChange={handleChange}></input>
+                <label>Odds</label>
+                <input id="odds" type="text" name="odds" placeholder="-110" value={formData.odds} onChange={handleChange}></input>
+                <label>League</label>
+                <input id="league" type="text" name="league" placeholder="MLB" value={formData.league} onChange={handleChange}></input>
+                <label>Result</label>
+                <input id="result" type="text" name="result" placeholder="Leave blank if no result" value={formData.result} onChange={handleChange}></input>
+                <label>Units Won or Lost</label>
+                <input id="unitsChange" type="text" name="units_change" placeholder="Ex: +1.3 or -1.15" value={formData.unitsChange} onChange={handleChange}></input>
+                <label>Segment</label>
+                <input id="segment" type="text" name="segment" placeholder="Ex: Take It to the Bank" value={formData.segment} onChange={handleChange}></input>
+                <button type="submit">Add Bet</button>
+                <label>Add Another Bet?</label>
+                <input id="checkbox" type="checkbox" checked={checked} onChange={handleCheck}></input>
+            </form>}
         </div>
     )
 }
