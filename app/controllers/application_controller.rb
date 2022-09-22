c3bebc98319cb499cf87d8021987787383be8174
Michael <michael.c.montgomery@gmail.com>
2022-09-22 16:35:25 -0500

    added category to form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,23 +2,23 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/people" do
+  get '/people' do
     people = Person.all
     people.to_json(include: :bets)
   end
 
-  post "/people" do
+  post '/people' do
     new_person = Person.create(name:params[:name], include: {bets: {description:params[:description], odds:params[:odds], league:params[:league], bet_type:params[:bet_type], result:params[:result], units_change:params[:units_change], segment:params[:segment]}})
     new_person.to_json
   end
 
-  delete "/people/:id" do
+  delete '/people/:id' do
     person_bet = Person.find(params[:id])
     person_bet.destroy
     person_bet.to_json
   end
 
-  patch "/people/:id" do
+  patch '/people/:id' do
     person_bet = Person.find(params[:id])
     person_bet.update(name:params[:name], include: {bets: {description:params[:description], odds:params[:odds], league:params[:league], bet_type:params[:bet_type], result:params[:result], units_change:params[:units_change], segment:params[:segment]}})
     person_bet.to_json
